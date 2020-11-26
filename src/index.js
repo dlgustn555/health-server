4a41189f57a51ecaf90203fe463ed77d0404e067
dlgustn555 <dlgustn555@naver.com>
2020-11-26 17:52:18 +0900

    #8 koa-bodyparser 적용

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 const Koa = require('koa')
+const bodyParser = require('koa-bodyparser')
 const mongoose = require('mongoose')
 const router = require('./routes')
 
-mongoose.connect('mongodb://localhost:27017/health', {useUnifiedTopology: true, useNewUrlParser: true})
+mongoose.connect('mongodb://localhost:27017/health', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
 
-const db = mongoose.connection;
-db.on('error', function() {
+const db = mongoose.connection
+db.on('error', function () {
     console.log('[MONGODB] Connection ERROR!!')
-});
-db.once('open', function() {
+})
+db.once('open', function () {
     console.log('[MONGODB] Connection SUCCESS!!')
-});
+})
 
 const server = new Koa()
 
-server.use(router.routes());
+server.use(bodyParser())
+server.use(router.routes())
 
 server.use(router.routes())
 server.listen(7500, () => {
