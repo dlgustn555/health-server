33ebdc353811d2ee4886f08ec6f6b9c550ebf0d0
hyunsu.lee <hyunsu.lee@navercorp.com>
2020-12-09 22:25:37 +0900

    #4 원격mongodb 접속

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ const router = require('./routes')
 
 // Node.js의 native Promise 사용
 mongoose.Promise = global.Promise
-
-mongoose.connect('mongodb://localhost:27017/health', {
+const dbUrl = 'mongodb+srv://dlgustn555:GodSent85@healthcluster.34gq3.mongodb.net/health?retryWrites=true&w=majority'
+// 'mongodb://localhost:27017/health'
+mongoose.connect(dbUrl, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useFindAndModify: false
